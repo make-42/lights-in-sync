4648cb0f6c31f1dac8d6749b01c11dfb6e1ecde6
Louis Dalibard <louis.dalibard@gmail.com>
2025-12-06 20:53:37 +0100

    init: init commit

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   in {
     packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
       pname = "lights-in-sync";
-      version = "0-unstable-2025-12-06";
+      version = "1-unstable-2025-12-06";
 
       src = pkgs.fetchFromGitHub {
         owner = "make-42";
