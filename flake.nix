941df6c90734f7d1f5d26717aa108e073ea4cf38
Louis Dalibard <louis.dalibard@gmail.com>
2025-12-06 20:52:58 +0100

    init: init commit

--- flake.nix
+++ flake.nix
@@ -19,8 +19,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "make-42";
         repo = "lights-in-sync";
-        rev = "2fc983e772acdcc40b4e8e75d276568b69a09792";
-        hash = "sha256-D3BobeuHgaSfGijuuHTJItMHeO/AydBFv0QzLfQrIdo=";
+        rev = "71bf691b23107b548f794caad1cd5e6e487d49be";
+        hash = "sha256-EYJDp1en7fRg6o21b7dmp4QPaWWssy6JUd8WJVaNJh8=";
       };
 
       cargoHash = "sha256-+A5JK0SAhLf1g4wwPNH5j2wqZWXHqU9kwE6ddtG0q7k=";
@@ -30,7 +30,7 @@
         homepage = "https://github.com/make-42/lights-in-sync";
         license = pkgs.lib.licenses.mit;
         maintainers = with pkgs.lib.maintainers; [];
-        mainProgram = "flow";
+        mainProgram = "lights-in-sync";
       };
     };
 
